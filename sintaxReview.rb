35dee7d3c9fd36bf6e3be171bda206bec9c9cf5f
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-10-06 21:13:56 +0200

    adds book references; first dialog to Ruby from Terminal

--- sintaxReview.rb
+++ sintaxReview.rb
@@ -1,2 +1,11 @@
 names = ['name1', 'name2']
 names.each { |n| puts "Name is #{n}"}
+=begin Ruby is the language that taught me to smile again
+from terminal:
+$ irb
+irb(main):001:0> puts 'hello, world'
+hello, world
+=> nil
+irb(main):002:0>
+=ends
+
